b1668ce692ba428dc02d6c27da5c75ef4ddede03
qiujiahong <qiujiahongde@163.com>
2020-02-04 22:22:11 +0800

    更新安装脚本

--- docs/docker/install_docker.sh
+++ docs/docker/install_docker.sh
@@ -5,41 +5,42 @@ echo "start to dowload docker...."
 if [ -a "docker-18.03.1-ce.tgz" ];then
         echo "found docker install file"
 else
-        echo "not docker install file"
+        echo "can't find docker install file"
+        yum install -y wget 
+        wget https://download.docker.com/linux/static/stable/x86_64/docker-18.03.1-ce.tgz
 fi
-# yum install -y wget 
-# wget https://download.docker.com/linux/static/stable/x86_64/docker-18.03.1-ce.tgz
-# tar -xvf 
-# chmod +x docker-18.03.1-ce/*
-# cp docker-18.03.1-ce/* /usr/local/bin/
-# cat > /usr/lib/systemd/system/docker.service <<"EOF"
-# [Unit]
-# Description=Docker Application Container Engine
-# Documentation=http://docs.docker.io
 
-# [Service]
-# Environment="PATH=/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin"
-# EnvironmentFile=-/run/flannel/docker
-# ExecStart=/usr/local/bin/dockerd --log-level=error $DOCKER_NETWORK_OPTIONS
-# ExecReload=/bin/kill -s HUP $MAINPID
-# Restart=on-failure
-# RestartSec=5
-# LimitNOFILE=infinity
-# LimitNPROC=infinity
-# LimitCORE=infinity
-# Delegate=yes
-# KillMode=process
+tar -xvf docker-18.03.1-ce.tgz
+chmod +x docker-18.03.1-ce/*
+cp docker-18.03.1-ce/* /usr/local/bin/
+cat > /usr/lib/systemd/system/docker.service <<"EOF"
+[Unit]
+Description=Docker Application Container Engine
+Documentation=http://docs.docker.io
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
-# # 国内dockerhub加速   
-# cat <<'EOF' > /etc/docker/daemon.json
-# {
-# "registry-mirrors": ["https://registry.docker-cn.com"]
-# }
-# EOF
+[Service]
+Environment="PATH=/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin"
+EnvironmentFile=-/run/flannel/docker
+ExecStart=/usr/local/bin/dockerd --log-level=error $DOCKER_NETWORK_OPTIONS
+ExecReload=/bin/kill -s HUP $MAINPID
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+Delegate=yes
+KillMode=process
 
-# systemctl restart docker
-# systemctl enable docker
+[Install]
+WantedBy=multi-user.target
+EOF
+# 国内dockerhub加速   
+cat <<'EOF' > /etc/docker/daemon.json
+{
+"registry-mirrors": ["https://registry.docker-cn.com"]
+}
+EOF
+
+systemctl restart docker
+systemctl enable docker
 
