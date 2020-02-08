e4f87566212a758a54cfdd34973ee378331fb4d0
qiujiahong <qiujiahongde@163.com>
2020-02-08 21:45:51 +0800

    完成脚本

--- docs/Linux/content/install/jdk11_install.sh
+++ docs/Linux/content/install/jdk11_install.sh
@@ -20,7 +20,8 @@ rpm -qa|grep java | grep openjdk | grep -v grep | xargs -r yum -y remove
 
 mkdir -p $JAVA_HOME_VAR
 
-tar -xzvf jdk-*-linux-x64.tar.gz -C /apps/
+
+tar -xzvf jdk-11*-linux-x64*.tar.gz -C /apps/
 ln -s /apps/jdk* /apps/jdk
 
 sed -i '/JAVA_HOME_VAR/'d /etc/profile 
