906134466b4d1a89961bc72ae399647730f0fe84
qiujiahong <qiujiahongde@163.com>
2020-02-08 21:41:08 +0800

    错误提示

--- docs/Linux/content/install/jdk11_install.sh
+++ docs/Linux/content/install/jdk11_install.sh
@@ -7,7 +7,7 @@ echo "check the jdk file...."
 if [ -a "jdk-11"*"-linux-x64.tar.gz" ];then
         echo "found jdk install file"
 else
-        echo "can't find jdk 11 install file. Please download it in the following url."
+        echo "ERROR: can't find jdk 11 install file. Please download it in the following url. and run the command again."
         echo "Oracle: https://www.oracle.com/technetwork/java/javase/downloads/jdk11-downloads-5066655.html"
         echo "Baidu cloud: https://pan.baidu.com/s/1EgLncH-wOZdLpjya4xcHcA  Code:e7el"
         exit 1
